862d66c6b7cee50d1fcc942ad0b9c478f2413eda
Diana <diapa22@gmail.com>
2024-05-14 12:30:18 -0500

    organitation of code main and style

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+import {elements, reviews} from "./src/reviews/reviews.js";
+import {mostrar} from "./tareadiana.js";
+
+
+const app = document.getElementById('app');
+
+// Creación de los elementos del DOM
+elements(app);
+// Llama a la función reviews después de haber agregado los elementos al DOM
+reviews();
+
+mostrar(2);
