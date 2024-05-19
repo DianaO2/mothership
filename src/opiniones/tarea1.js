c3288e19a56f0ac6198a98a80e2817316431ad4a
lesre <leslieege2@gmail.com>
2024-05-18 22:20:31 -0300

    agregando stilos y agrege carpeta img dentro de opiniones

--- src/opiniones/tarea1.js
+++ src/opiniones/tarea1.js
@@ -1,49 +1,47 @@
-// Crear la tarjeta
 
+// Crear la tarjeta
 const cardReview = (
- comentario,imagen,rating
-) => {
+ comentario,imagen,rating) => {
   const card = `
-            <div class="card">
-              <div class="imagen-tarjeta">
-                  <img src=${imagen} alt="foto de perfil" class="slideshow-img"/>
-              </div>
-              <div class="text-rating">
-                  <p>${rating}</p>
-                  <p class="text">${comentario}</p>
-              </div>               
-            </div>
-          </div>
-      `;
+    <div class="contenedor-card">
+      <div class="card-respuesta"
+               <div class="imagen-tarjeta">
+                 <img src=${imagen} alt="foto de perfil" class="slideshow-img"/>
+               </div>
+          <div class="text-rating">
+            <p>${rating}</p>
+            <p class="text">${comentario}</p>
+          </div> <br><br><br><br><br>
+       <div class="cuadro">
+       <div class="imagen-tarjeta">
+       <img src="opiniones/imagen/2.jpg  alt="foto de perfil"/>
+     </div>
+           <p>Nombre del emprendimiento</p>
+          <div class="responder"> 
+            <div class="gracias-opinion"> 
+            <p>Gracias por dejarnos tu opinión</p>
+          </div>    
+        </div>
+      </div>
+    </div> 
+   `;
   return card;
 };
-
 export function mostrarOpinion(app,comentario,imagen,rating){
-  
   const tarjeta = document.createElement("div");
   tarjeta.classList.add("container-card")
-  
-  
- 
- tarjeta.insertAdjacentHTML("beforeend", cardReview(comentario,imagen,rating))
- app.appendChild(tarjeta)
-
-
-
- 
-
+  tarjeta.insertAdjacentHTML("beforeend", cardReview(comentario,imagen,rating))
+app.appendChild(tarjeta)
   }
 
 
   
-
-/*
-// Crear botón de responder
+/*// Crear botón de responder
 const botonResponder = document.createElement("button");
 botonResponder.textContent = "Responder";
-botonResponder.classList.add("botonVioleta");
+botonResponder.classList.add("botonVioleta");*/
 
-// Crear botón de cancelar
+/*// Crear botón de cancelar
 const botonCancelar = document.createElement("button");
 botonCancelar.textContent = "Cancelar";
 botonCancelar.classList.add("botonBlanco");
@@ -53,5 +51,5 @@ botonCancelar.classList.add("botonBlanco");
 app.appendChild(casillaRespuesta);
 app.appendChild(botonResponder);
 app.appendChild(botonCancelar);
-  };*/
-
+  
+*/
